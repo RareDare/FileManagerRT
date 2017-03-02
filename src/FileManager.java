4051b9dc907e40c4ddb1d77ec1bf6890411b8a1c
RareDare <ruslantkachenko123@gmai.com>
2017-03-02 17:03:25 +0200

    Initial commit

---
+++ src/FileManager.java
@@ -0,0 +1,21 @@
+/**
+ * Created by tester on 3/2/17.
+ */
+public class FileManager {
+    public static void main(String[] args) {
+
+    }
+
+    public void createFile(){
+
+    }
+    public void deleteFile(){
+
+    }
+    public void editName(){
+
+    }
+    public void findName(){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tester on 3/2/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Create File");
+        System.out.println("2. Delete File");
+        System.out.println("3. Edit Name");
+        System.out.println("4. Find Name");
+
+    }
+}
